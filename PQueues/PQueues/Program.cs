b18777052db53714df9c698b7f23b0da24edff49
photon318 <photon318@gmail.com>
2017-10-16 12:29:51 +0300

    Some garbage

---
+++ PQueues/PQueues/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace PQueues
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to parallel queue processing tests.");
+            Console.WriteLine($"Running on {Environment.ProcessorCount} CPU's.");
+
+
+            // FIFO test with tasks
+
+
+            // Assign
+
+            var list = new List<int>();
+            var tasks = new List<Task>();
+            var range = Enumerable.Range(0, 1000);
+ 
+            // Act
+
+            foreach (var number in range)
+            {
+                //Console.Write($"{number} ");
+
+                tasks.Add(Task.Factory.StartNew(() => list.Add(number), TaskCreationOptions.PreferFairness));
+            }
+
+            Task.WhenAll(tasks);
+            Console.WriteLine($"Sequence: {range.SequenceEqual(list)} ");
+
+            // Assert
+
+            var queueserial = new SerialQueue();
+            var listserial = new List<int>();
+            var tasksserial = new List<Task>();
+            var rangeserial = Enumerable.Range(0, 10000);
+
+            // Act
+
+            foreach (var number in rangeserial)
+            {
+                tasksserial.Add(queueserial.Enqueue(() => listserial.Add(number)));
+            }
+
+            Task.WhenAll(tasksserial);
+
+            // Assert
+
+            //Console.WriteLine($"Sequence: {rangeserial.SequenceEqual(listserial)} ");
+
+           // Task t = new Task.Factory.StartNew();
+
+
+
+            const int count = 10000;
+            var queuemulti = new SerialQueue();
+            var listmulti = new List<int>();
+
+            // Act
+
+            var counter = 0;
+            //bool done = false;
+            for (int i = 0; i < count; i++)
+            {
+                Task.Run(() => {
+                    queuemulti.Enqueue(() => listmulti.Add(counter++));
+                });
+            }
+
+            //done = true;
+
+           // while (list.Count != count) { };
+
+            // Assert
+
+            //Console.WriteLine($"Sequence: {listmulti.SequenceEqual(Enumerable.Range(0, count))}");
+        }
+    }
+}
---
+++ PQueues/PQueues/SerialQueue.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace PQueues
+{
+    public class SerialQueue
+    {
+        readonly object _locker = new object();
+        WeakReference<Task> _lastTask;
+
+        public Task Enqueue(Action action)
+        {
+            lock (_locker)
+            {
+                return Enqueue<object>(() => {
+                    action();
+                    return null;
+                });
+            }
+        }
+
+        public Task<T> Enqueue<T>(Func<T> function)
+        {
+            lock (_locker)
+            {
+                Task lastTask = null;
+                Task<T> resultTask = null;
+
+                if (_lastTask != null && _lastTask.TryGetTarget(out lastTask))
+                {
+                    resultTask = lastTask.ContinueWith(_ => function());
+                }
+                else
+                {
+                    resultTask = Task.Run(function);
+                }
+
+                _lastTask = new WeakReference<Task>(resultTask);
+                return resultTask;
+            }
+        }
+    }
+}
